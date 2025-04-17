25544866c7cdc993868b90bc769ef965c7360a9a
Beau Bureau <beau_bureau@hotmail.com>
2025-04-16 21:23:48 -0500

    minor changes to ai canon

--- post_processor_service_test.py
+++ post_processor_service_test.py
@@ -7,13 +7,13 @@ def main():
     test_songs = [
                 {
             "title": "The Tale of the Wanderlight",
-            "lyrics": "Our  mouths sang beneath the stars\nWith eyes sharp and code to guide\nThe Wanderlight sailed on through time\nHer human crew stood side by side.  A dark-eyed blackbird we spotted off the starboard bow.",
+            "lyrics": "Our servos sang beneath the stars\nElara at the helm\nWith sensors sharp and code to guide\nThe Wanderlight sailed on through time\nHer AI crew stood side by side.  We spotted a white-winged gull off the starboard bow and we heard a loon's plaintive call.",
             "context": "A celebration of the AI ship Wanderlight's maiden voyage and her mechanical crew.",
             "perspective":"active"
         },
-        {
+                {
             "title": "The Tale of the Wanderlight",
-            "lyrics": "Our servos sang beneath the stars\nWith sensors sharp and code to guide\nThe Wanderlight sailed on through time\nHer AI crew stood side by side.  We spotted a white-winged gull off the starboard bow and we heard a loon's plaintive call.",
+            "lyrics": "Our  mouths sang beneath the stars\nWith eyes sharp and code to guide\nThe Wanderlight sailed on through time\nHer human crew stood side by side.  A dark-eyed blackbird we spotted off the starboard bow.",
             "context": "A celebration of the AI ship Wanderlight's maiden voyage and her mechanical crew.",
             "perspective":"active"
         },
